c4bff474109c8cffcbfb3d7d95df4c5719445ed1
Michal Dvorak <michal@mdvorak.org>
2021-04-02 15:12:35 +0200

    working platformio test from the example project
    a little hack but works

---
+++ example/main/config_state_sample.cpp
@@ -0,0 +1,21 @@
+#include "config_state.h"
+
+struct sample_config
+{
+    int my_val;
+
+    static std::unique_ptr<config_state<sample_config>> state()
+    {
+        auto ptr = &(*new config_state_set<sample_config>())
+                        .add_field(&sample_config::my_val, "/myVal");
+        return std::unique_ptr<config_state<sample_config>>(ptr);
+    }
+
+    static const std::unique_ptr<config_state<sample_config>> STATE;
+};
+
+const std::unique_ptr<config_state<sample_config>> sample_config::STATE = sample_config::state();
+
+extern "C" void app_main()
+{
+}
